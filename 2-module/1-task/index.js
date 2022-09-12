f58d8711d4e05312b40c46a5a6aca0ae4b5898ca
chagrin2002 <mapalian@gmail.com>
2022-09-12 22:01:26 +0300

    задачи занятия 2

--- 2-module/1-task/index.js
+++ 2-module/1-task/index.js
@@ -1,3 +1,10 @@
 function sumSalary(salaries) {
-  // ваш код...
-}
+  let sum = 0;
+  for (let key in salaries) 
+  {
+   if (isNaN(salaries[key]) === true || isFinite(salaries[key]) === false)
+   continue;
+   sum += salaries[key];
+  }
+  return sum;
+};
--- 2-module/3-task/index.js
+++ 2-module/3-task/index.js
@@ -1,6 +1,22 @@
-let calculator = {
-  // ваш код
-};
+let calculator = 
+{
+  a:0,
+  b:0,
 
+  sum(a,b) 
+  {
+    return this.a + this.b;
+  },
+  mul(a,b)
+  {
+    return this.a * this.b;
+  },
+  read(a,b)
+  {
+    this.a = a;
+    this.b = b;
+  }
+ 
+};
 // НЕ УДАЛЯТЬ СТРОКУ, НУЖНА ДЛЯ ПРОВЕРКИ
 window.calculator = calculator; // делает ваш калькулятор доступным глобально
